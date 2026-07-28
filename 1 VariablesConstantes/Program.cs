f99f366299ed382ceb76cdd040779d11fee72ec9
Ale <alejandra.romero@upb.edu.co>
2026-07-28 13:35:51 -0500

    mi primer commit

---
+++ 1 VariablesConstantes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //aquí escribo el código fuente
+            string nombre = "Alejandra Romero";
+            Console.WriteLine("Bienvenido " + nombre +  " al curso de Fundamentos de Programación");
+        }
+    }
+}
