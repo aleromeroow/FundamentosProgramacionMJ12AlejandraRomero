f9414b46faef0b10bdab8a350f30d0f62c191fd7
Ale <alejandra.romero@upb.edu.co>
2026-08-13 15:00:19 -0500

    CONDICIONALES MULTIPLES YUPIIII

---
+++ 6 CondicionalesMultiples/6 CondicionalesMultiples/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace _6_CondicionalesMultiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicionales multiples 
+            int respuesta = 0;
+            Console.WriteLine("Menú");
+            Console.WriteLine("1.Opción 1                   2.Opción 2");
+            Console.WriteLine("3.Opción 3                   4.Opción 4");
+            Console.WriteLine("5.Opción 5");
+            Console.WriteLine("_______________________________________");
+            Console.WriteLine("Elija una opción del menú");
+
+            switch (respuesta)
+            {
+                case 1:
+                    Console.WriteLine("Eligió la opción 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Eligió la opción 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Eligió la opción 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Eligió la opción 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Eligió la opción 5");
+                    break;
+                default:
+                    Console.WriteLine("Elija una opción válida");
+                    break;
+            }
+        }
+    }
+}
---
+++ 6 CondicionalesMultiples/6 CondicionalesMultiples/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("6 CondicionalesMultiples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("6 CondicionalesMultiples")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("b805fd8a-0730-40e0-a0e1-44f9c8915ece")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
